fd476ad71ab5abafc008f9f330a86140f43886cf
hashemmousavi <hashemcafebazar@gmail.com>
2016-06-24 15:37:51 -0700

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -225,6 +225,7 @@ function create_config( )
     "set",
     "get",
     "broadcast",
+    "plugins",
     "invite",
     "all",
     "leave_ban",
@@ -232,7 +233,7 @@ function create_config( )
 	"whitelist",
 	"msg_checks"
     },
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {96395383},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v4
 An advanced administration bot based on TG-CLI written in Lua
